134259da3c3cceafed84edad6c0f740df5650d32
pouyan kheradmand <85466572+puoyan@users.noreply.github.com>
2021-08-11 16:22:03 +0200

    Add files via upload

---
+++ exPHPday3team/index.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="/css/style.css">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<title>Document</title>
+</head>
+<body>
+<?php 
+////////live code/////////////////
+// for($i = 0; $i <= 10 ; $i++ ){
+// 	echo $i . "<br>";
+// }
+
+
+
+//////////////////////////////////
+
+// $z = 0;
+// while($z <= 10){
+// 	echo $z . "<br>";
+// $z++;	
+// }
+
+
+//////////////////////////////////
+
+
+// $z = 50;   //شرط اجرا نمیشه چون از اول شرط و چک میکنه
+// while($z <= 10){
+// 	echo $z . "<br>";
+// $z++;	
+// }
+
+
+
+// $y = 50;
+// do{
+//     echo $y . "<br>";
+//     $y++;	
+// }while($y <= 10);
+
+
+
+
+//////////////////////////////////
+
+
+//  $arr = array(2,3,5,7,4);
+ 
+//  foreach($arr as $val){
+// 	 echo $val . "<br>";
+//  }
+
+
+
+
+// $arr = array(2,3,5,7,4);
+ 
+// foreach($arr as $key => $val){
+// 	echo "the index of the elment is" .$key. "and the value".$val . "<br>";
+// }
+
+
+
+// $arr = array(2,3,5,7,4);
+ 
+// foreach($arr as $key => $val){
+// 	if($val == 5) break;
+// 	echo "the index of the elment is" .$key. "and the value".$val . "<br>";
+// }
+
+
+
+
+// $arr = array(2,3,5,7,4,5);
+ 
+// foreach($arr as $key => $val){
+// 	if($val == 5) continue;
+// 	echo "the index of the elment is" .$key. "and the value".$val . "<br>";
+// }
+
+
+
+
+//////////////// Eercise 1//////////////////
+// $Name = 1;
+// while($Name <= 50){ 
+// 	echo $Name. "Pouyan  <br>";
+// $Name++;	
+// }
+//////////////// Eercise 2//////////////////
+
+
+// $arr = array(1,2,3,4,5,6,7,8,9,10);
+ 
+// foreach($arr as $val){
+	
+// 	echo $val. "<br>";
+// }
+
+//////////////// Eercise 3 //////////////////
+// $arr = array(1,2,3,4,5,6,7,8,9,10,20);
+// function arr($funny){
+//  $max = max($funny);
+// 	echo $max;
+// }
+//  arr($arr); 
+
+//////////////// Eercise 4 //////////////////
+
+// for($i=1 ; $i < 101 ; $i++){
+//  if ($i%3 == 0 AND $i%5 == 0){
+// 	 echo "Full-Stack </br>";
+// 	}else{
+
+	
+// 	if ($i%3 == 0){
+// 		echo "Back-End </br>";
+// 	}
+// 	elseif($i%5 == 0){
+// 		echo "Front-End </br>";	
+// 	}
+// 	else{
+// 		echo $i. "</br>";
+// 	}
+// }
+// }
+//////////////// Eercise 4 //////////////////
+
+
+
+
+?>
+</body>
+</html>
